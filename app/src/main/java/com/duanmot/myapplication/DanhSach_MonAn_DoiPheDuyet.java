8ab008a1f1f860aed7f1f9ab60558d50e1378b7b
DragonHL <danghalong1991@gmail.com>
2020-05-23 16:08:42 +0700

    Version 1

---
+++ app/src/main/java/com/duanmot/myapplication/DanhSach_MonAn_DoiPheDuyet.java
@@ -0,0 +1,14 @@
+package com.duanmot.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DanhSach_MonAn_DoiPheDuyet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_danh_sach__mon_an__doi_phe_duyet);
+    }
+}
---
+++ app/src/main/java/com/duanmot/myapplication/ManHinhChinh_Admin.java
@@ -0,0 +1,42 @@
+package com.duanmot.myapplication;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+
+public class ManHinhChinh_Admin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.manhinh_chinh);
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragmentActivity()).commit();
+
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment = null;
+                    switch (menuItem.getItemId()) {
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragmentActivity();
+                            break;
+                        case R.id.nav_user:
+                            selectedFragment = new Account_Admin_FragmentActivity();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                    return true;
+                }
+            };
+}
